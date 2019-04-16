6f8e4afcaeadba3aec6da9616d59ab8a87550052
Terry <guanghuan3@163.com>
2019-04-16 21:48:21 +0800

    【设计模式：架构师审美】 springIOC时序

---
+++ homework/src/main/java/com/guanghuan3/homework/designpattern/proxy/Company.java
@@ -0,0 +1,12 @@
+package com.guanghuan3.homework.designpattern.proxy;
+
+/**
+ * Created by Administrator
+ * on 2019/4/16.
+ */
+public class Company {
+
+    public void build() {
+        System.err.println("团购售楼");
+    }
+}
