6bb32481417def5967e9fd72cd4d154c2de56cf3
Terry <guanghuan3@163.com>
2019-04-16 18:37:51 +0800

    【设计模式：架构师审美】 springIOC时序

--- homework/src/main/java/com/guanghuan3/homework/designpattern/proxy/staticproxy/StaticProxyTest.java
+++ homework/src/main/java/com/guanghuan3/homework/designpattern/proxy/staticproxy/StaticProxyTest.java
@@ -6,7 +6,9 @@ package com.guanghuan3.homework.designpattern.proxy.staticproxy;
  * 静态代理
  * 在被代理的目标对象执行操作的前后进行加强
  *
- * 缺点：当被代理的对象类型增加时需要新增加对应的代理对象，量大时去实现工作量直接爆炸
+ * 缺点：
+ * 1、当被代理的对象类型增加时需要新增加对应的代理对象，量大时去实现工作量直接爆炸
+ * 2、当被代理对象可执行的操作增加且增强的功能一致时，代理方法需要修改代码对应增加
  * on 2019/4/16.
  */
 public class StaticProxyTest {
